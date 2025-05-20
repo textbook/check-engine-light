692afc8fd9a1cefe0d1c7eaaa1c68c0d3d6f2fb5
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2025-05-20 10:59:27 +0100

    Add versioning script

---
+++ bin/version.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+if [ $# -ne 1 ]; then
+  echo "usage: ./bin/version.sh <version>"
+  echo "This will update the version for all packages"
+  exit 1
+fi
+
+if [ -n "$(git status --porcelain --untracked-files=no)" ]; then
+  echo 'Git working directory not clean.'
+  exit 1
+fi
+
+HERE="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+ROOT="$HERE/.."
+
+commitAndTag() {
+  git add \
+    "$ROOT/package.json" \
+    "$ROOT/package-lock.json" \
+    "$ROOT/packages/*/package.json"
+  git commit --message "$1"
+  git tag "$1"
+}
+
+useNpm() {
+  npm --prefix="$ROOT" "$@"
+}
+
+VERSION="$(useNpm version --no-git-tag-version "$1")"
+useNpm --workspace='check-engine-light' version "$VERSION"
+useNpm install --package-lock-only
+commitAndTag "$VERSION"
